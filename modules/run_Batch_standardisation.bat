e52f4ceb479fc49f70d96330ab04806b7988e33f
Anael-Cabrol <jupyter@vm-py-formateur.europe-west1-b.c.epsi-tech-dsc-formation-202005.internal>
2022-06-17 11:28:43 +0200

    REFACTO : Suppression des checkpoints dans Git et ajout d'un gitignore

--- modules/run_Batch_standardisation.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\dgr\AppData\Local\Continuum\anaconda3\python.exe" "C:\Users\dgr\OneDrive - Publicis Groupe\Documents\Formations\SCU Python\NEW\Python-ML\modules\standardisation.py"
-pause
